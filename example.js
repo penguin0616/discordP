48140a140e87d4c83bfcd835af31296f94ea31f7
Riad Abboud <riadabboud0616@gmail.com>
2017-11-18 18:40:19 -0600

    updated example.js

--- example.js
+++ example.js
@@ -1,16 +1,18 @@
-const discord = require("PATH_TO_MAIN_FOLDER/main.js");
-discord.connect({token: ""}) // can also use {email: "", password: ""}
+const discordp = require("PATH_TO_MAIN_FOLDER/main.js");
 
+var bot = new discordp({
+	debug: true,
+	shardId: 1,
+	shardCount: 1
+});
 
-discord.events.on('LOGIN_SUCCESS', (d) => {
-	console.log("Successfully logged into discord.");
+bot.connect({token: ""}) // can also use {email: "example@example.com", password: "superSecret123"}
+
+bot.events.on('GATEWAY_READY', () => {
+	console.log('Logged in as: ' + bot.user.fullName)
 })
 
-discord.events.on('LOGIN_FAIL', (d) => {
-	console.log('Failed to login to discord.');
-})
-
-discord.events.on('MESSAGE_CREATE', (msg) => {
+bot.events.on('MESSAGE_CREATE', (msg) => {
 	if (msg.content.startsWith("/ping")==true) {
 		msg.channel.sendMessage('pong');
 	}
